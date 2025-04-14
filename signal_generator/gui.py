c30cb532909067148c228d3317da27eb737b42b7
rh8991 <103560124+rh8991@users.noreply.github.com>
2025-04-15 01:23:18 +0300

    Initial commit

--- signal_generator/gui.py
+++ signal_generator/gui.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg
 
 def create_window():
     layout = [
-        [sg.Text('Signal Type:'), sg.Combo(['Sine', 'Square'], default_value='Sine', key='type')],
+        [sg.Text('Signal Type:'), sg.Combo(['Sine', 'Square', 'Triangle'], default_value='Sine', key='type')],
         [sg.Text('Frequency (Hz):'), sg.InputText('440', key='freq')],
         [sg.Text('Amplitude (0-1):'), sg.InputText('0.5', key='amp')],
         [sg.Text('Duration (s):'), sg.InputText('2', key='duration')],
