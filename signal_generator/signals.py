c158fc4e7563049928ee1dea2f440bf2dcfd419f
rh8991 <103560124+rh8991@users.noreply.github.com>
2025-04-15 20:08:27 +0300

    Initial commit

--- signal_generator/signals.py
+++ signal_generator/signals.py
@@ -2,12 +2,12 @@ import numpy as np
 
 def generate_sine(freq, amp, phase, offset, duration, fs):
     t = np.linspace(0, duration, int(fs* duration), endpoint=False)
-    return t, amp*np.sin(2 * np.pi * freq * t + phase)
+    return t, amp*np.sin(2 * np.pi * freq * t + phase) + offset
 
 def generate_square(freq, amp, phase, offset, duration, fs):
     t = np.linspace(0, duration, int(fs * duration), endpoint=False)
-    return t, amp * np.sign(np.sin(2 * np.pi * freq * t + phase))
+    return t, amp * np.sign(np.sin(2 * np.pi * freq * t + phase)) + offset
 
 def generate_triangle(freq, amp, phase, offset, duration, fs):
     t = np.linspace(0, duration, int(fs * duration), endpoint=False)
-    return t, amp * (2 * np.abs(2 * (t * freq - np.floor(t * freq + 0.5))) - 1) + phase
+    return t, amp * (2 * np.abs(2 * (t * freq - np.floor(t * freq + 0.5))) - 1) + phase + offset
