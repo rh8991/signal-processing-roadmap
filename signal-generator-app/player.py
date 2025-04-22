627fdb213b73dc5cfb75040c39a07b760d912611
rh8991 <103560124+rh8991@users.noreply.github.com>
2025-04-22 15:55:39 +0300

    Initial commit

--- signal-generator-app/player.py
+++ signal-generator-app/player.py
@@ -1,5 +1,16 @@
-import sounddevice as sd
+# signal-generator-app/player.py
+# This module is responsible for playing the generated audio signal.
+import simpleaudio as sa
+import numpy as np
 
 def play_signal(signal, fs):
-    sd.play(signal, fs)
-    sd.wait()
+    # Ensure that highest value is in 16-bit range
+    audio = signal * (2**15 - 1) / np.max(np.abs(signal))
+    # Convert to 16-bit data
+    audio = audio.astype(np.int16)
+
+    # Start playback
+    play_obj = sa.play_buffer(audio, 1, 2, fs)
+
+    # Wait for playback to finish before exiting
+    play_obj.wait_done()
